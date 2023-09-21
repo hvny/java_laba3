6f1e8bc6212d32660c1afd334781f34fc0609aa3
Vadim Elsukov <elsukov.vadim@gmail.com>
2023-09-21 11:21:16 +0300

    done

--- laba3/src/main/java/org/example/Main.java
+++ laba3/src/main/java/org/example/Main.java
@@ -2,42 +2,75 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String[][] arr1 = {
+        String[][] arr1 = {                     //матрица
                 {"1", "2", "3", "4"},
                 {"1", "2", "3", "4"},
                 {"1", "2", "3", "4"},
                 {"1", "2", "3", "4"}
         };
 
-        try {
+        try {                       //запуск теста
             arrTest(arr1);
 
-        } catch (MyArraySizeException e) {
-            e.printStackTrace();
-
-        } catch (MyArrayDataException e){
+        } catch (MyArraySizeException | MyArrayDataException | MyHappyException e) {
             e.printStackTrace();
         }
-
     }
 
-    static void arrTest(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+    static void arrTest(String[][] arr) throws MyArraySizeException, MyArrayDataException, MyHappyException {
         int sum = 0;
 
         if (arr.length != 4) throw new MyArraySizeException("Неверное количество строк");
 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i].length != 4) throw new MyArraySizeException("Неверное количество столбцов");
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + " ");
+
+            for (int k = 0; k < arr[i].length; k++) {
+                //System.out.print(arr[i][k] + " ");
+
                 try {
-                    sum += Integer.valueOf(arr[i][j]);
+                    sum += Integer.valueOf(arr[i][k]);
                 } catch (NumberFormatException e) {
-                    throw new MyArrayDataException(String.format("В позиции [%d][%d] исходного массива находится не целое число %s.", i, j, arr[i][j]));
+                    throw new MyArrayDataException(String.format("В позиции [%d][%d] исходного массива находится не целое число %s.", i+1, k+1, arr[i][k]));
                 }
             }
-            System.out.println();
+
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    if (CheckString(arr[i][j])) {           //проверка на счастливое число
+                        throw new MyHappyException("Some message");
+                    }
+                } catch(MyHappyException e) {
+                    System.err.println(new MyHappyException(String.format("В позиции [%d][%d] исходного массива находится счастливое число %s.", i+1, j+1, arr[i][j])));
+                    //System.err.println(String.format("В позиции [%d][%d] исходного массива находится счастливое число %s.", i+1, j+1, arr[i][j]));
+                }
+            }
+            //System.out.println();
         }
         System.out.println("Сумма элементов массива: " + sum);
+    }
+
+    public static boolean CheckString(String s) { //метод проверяющий счастливую строку
+        if (s.length() != 6) {
+            return false;
+        }
+        else {
+            int sumOfTheFirstGroup = 0;
+            int sumOfTheSecondGroup = 0;
+            String[] arrOfNums = s.split("");
+
+            for (int i = 0; i < arrOfNums.length/2; i++) {
+                sumOfTheFirstGroup += Integer.valueOf(arrOfNums[i]);
+                sumOfTheSecondGroup += Integer.valueOf(arrOfNums[arrOfNums.length - 1 - i]);
+            }
+            //System.out.println(String.format("Строка: " + s + "Сумма первых: " + sumOfTheFirstGroup + "Сумма последних: " + sumOfTheSecondGroup));
+
+            if (sumOfTheFirstGroup == sumOfTheSecondGroup) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
     }
 }
---
+++ laba3/src/main/java/org/example/MyHappyException.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class MyHappyException extends Exception{
+    MyHappyException(String message) {
+        super(message);
+
+    }
+}
